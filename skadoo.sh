aba6c44423bcf1e12ca42a201313b39a89741734
Rendyindo <rendyarya22@gmail.com>
2017-01-29 14:15:39 +0700

    miui

--- skadoo.sh
+++ skadoo.sh
@@ -63,7 +63,7 @@ checkfinishtime(){
 doshallow(){
     cd $DIR; mkdir -p $ROMNAME/shallow; cd $ROMNAME/shallow
 
-    expect -c 'spawn ~/bin/repo init -u https://github.com/ResurrectionRemix/platform_manifest -b lollipop5.1 --depth 1 -q --reference $DIR/$ROMNAME/full/; expect "Enable color display in this user account (y/N)?"; send -- "y\r"; expect eof'
+    expect -c 'spawn ~/bin/repo init -u https://github.com/MiCode/patchrom -b marshmallow --depth 1 -q --reference $DIR/$ROMNAME/full/; expect "Enable color display in this user account (y/N)?"; send -- "y\r"; expect eof'
 
     THREAD_COUNT_SYNC=8
 
@@ -109,7 +109,7 @@ doshallow(){
 dofull(){
     cd $DIR; mkdir -p $ROMNAME/full; cd $ROMNAME/full
 
-    expect -c 'spawn ~/bin/repo init -u https://github.com/ResurrectionRemix/platform_manifest -b lollipop5.1; expect "Enable color display in this user account (y/N)?"; send -- "y\r"; expect eof'
+    expect -c 'spawn ~/bin/repo init -u https://github.com/MiCode/patchrom -b marshmallow; expect "Enable color display in this user account (y/N)?"; send -- "y\r"; expect eof'
 
     THREAD_COUNT_SYNC=8
 
