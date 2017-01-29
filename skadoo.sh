8eba7d10b989071ecb2db53bc4ec3f4b5c725976
Rendyindo <rendyarya22@gmail.com>
2017-01-29 13:51:31 +0700

    test

--- skadoo.sh
+++ skadoo.sh
@@ -63,7 +63,7 @@ checkfinishtime(){
 doshallow(){
     cd $DIR; mkdir -p $ROMNAME/shallow; cd $ROMNAME/shallow
 
-    expect -c 'repo init -u $LINK -b $BRANCH --depth 1 -q --reference $DIR/$ROMNAME/full/; expect "Enable color display in this user account (y/N)?"; send -- "y\r"; expect eof'
+    expect -c 'repo init -u https://github.com/ResurrectionRemix/platform_manifest -b lollipop5.1 --depth 1 -q --reference $DIR/$ROMNAME/full/; expect "Enable color display in this user account (y/N)?"; send -- "y\r"; expect eof'
 
     THREAD_COUNT_SYNC=8
 
@@ -109,7 +109,7 @@ doshallow(){
 dofull(){
     cd $DIR; mkdir -p $ROMNAME/full; cd $ROMNAME/full
 
-    expect -c 'repo init -u $LINK -b $BRANCH; expect "Enable color display in this user account (y/N)?"; send -- "y\r"; expect eof'
+    expect -c 'repo init -u https://github.com/ResurrectionRemix/platform_manifest -b lollipop5.1; expect "Enable color display in this user account (y/N)?"; send -- "y\r"; expect eof'
 
     THREAD_COUNT_SYNC=8
 
