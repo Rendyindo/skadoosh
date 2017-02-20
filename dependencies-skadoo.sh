e03f2662af29f8634a29368c532dd1e98c170362
Rendyindo <rendyarya22@gmail.com>
2017-02-20 16:52:48 +0700

    Forgot to add i386 arch lol

--- dependencies-skadoo.sh
+++ dependencies-skadoo.sh
@@ -13,7 +13,7 @@
 #
 # Please maintain this if you use this script or any part of it
 #
-
+sudo dpkg --add-architecture i386
 sudo add-apt-repository ppa:wine/wine-builds
 sudo apt-get update
 sudo apt-get clean
